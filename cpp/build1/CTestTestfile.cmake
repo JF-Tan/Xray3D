73d7eecf9cd77b2dd604d132eb10555601ca2e97
Takk-Tanee <taneey0519@gmail.com>
2023-06-14 19:37:01 +0800

    123

--- cpp/build1/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tanee/GitHub/Xray3D/cpp
-# Build directory: /home/tanee/GitHub/Xray3D/cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(xray3d-test "COMMAND" "main")
-set_tests_properties(xray3d-test PROPERTIES  _BACKTRACE_TRIPLES "/home/tanee/GitHub/Xray3D/cpp/CMakeLists.txt;10;add_test;/home/tanee/GitHub/Xray3D/cpp/CMakeLists.txt;0;")
